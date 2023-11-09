1dfcae5128aa5645ba2b6e735a950cddaf1367ef
utkarsh-sohane <98829006+utkarsh-sohane@users.noreply.github.com>
2023-11-09 16:14:42 +0530

    Add files via upload

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Hello, I am Utkarsh sohane, and I'm currently pursuing a Bachelor's degree in Computer Science at VIT Bhopal. I'm passionate about technology, coding, and all things related to the digital world.
+          </p>
+          <p className="hero--section-description">
+          I'm currently a proud student at VIT Bhopal, where I'm honing my skills in computer science and preparing myself for a dynamic and ever-evolving industry. My coursework has provided me with a solid foundation in various programming languages, algorithms, and data structures.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Utkarsh</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Coding is not just a skill for me; it's a passion.
+            <br /> I love tackling challenging problems and turning ideas into working software.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
